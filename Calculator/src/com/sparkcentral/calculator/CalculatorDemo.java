63468b42672b95f3f694eed817af688daf5ea009
aethena <divyakali1288@gmail.com>
2015-07-14 19:57:24 -0400

    Adding skeletal code of Calculator application

--- Calculator/src/com/sparkcentral/calculator/CalculatorDemo.java
+++ Calculator/src/com/sparkcentral/calculator/CalculatorDemo.java
@@ -1,9 +1,21 @@
 package com.sparkcentral.calculator;
+
+import java.util.Scanner;
+
 /**
+ * This class is responsible for demonstrating different types of classes
  * 
  * @author aethena
  *
  */
 public class CalculatorDemo {
+	public static void main(String[] args) {
+		System.out.println("This is a demo of different kinds of calculations");
+		StandardCalculator standardCalculator = new StandardCalculator();
+		// use standard calculator
+		standardCalculator.runCalculator();
+		ScientificCalculator scientificCalculator = new ScientificCalculator();
+		// use Scientific Calculator
 
+	}
 }
